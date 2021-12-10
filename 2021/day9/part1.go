9790a34e6c3d1c608155b7f2fef06e55379b30dd
Marylia Gutierrez <maryliag@gmail.com>
2021-12-10 00:26:00 -0500

    day 9 solution

--- 2021/day9/part1.go
+++ 2021/day9/part1.go
@@ -37,51 +37,35 @@ func getMap() [][]int {
 
 func isLowPoint(heightMap [][]int, i int, j int) bool {
 	point := heightMap[i][j]
-	// Equals will check for the case where all values are the same.
-	equals := true
 	// Check up
 	if i-1 >= 0 {
-		if heightMap[i-1][j] < point {
+		if heightMap[i-1][j] <= point {
 			return false
-		}
-		if heightMap[i-1][j] != point {
-			equals = false
 		}
 	}
 
 	// Check down
 	if i+1 < len(heightMap) {
-		if heightMap[i+1][j] < point {
+		if heightMap[i+1][j] <= point {
 			return false
-		}
-		if heightMap[i+1][j] != point {
-			equals = false
 		}
 	}
 
 	// Check left
 	if j-1 >= 0 {
-		if heightMap[i][j-1] < point {
+		if heightMap[i][j-1] <= point {
 			return false
-		}
-		if heightMap[i][j-1] != point {
-			equals = false
 		}
 	}
 
 	// Check right
 	if j+1 < len(heightMap[0]) {
-		if heightMap[i][j+1] < point {
+		if heightMap[i][j+1] <= point {
 			return false
-		}
-		if heightMap[i][j+1] != point {
-			equals = false
 		}
 	}
 
-	// If none of the ones around are smaller and at least one of them
-	// is different, then this is the lower point.
-	return !equals
+	return true
 }
 
 func main() {
